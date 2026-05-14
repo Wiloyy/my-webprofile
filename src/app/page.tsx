24612f344a4c0a8fd2528018c06f24556f016f40
Wiloyy <mat.ams2006@gmail.com>
2026-05-14 20:58:56 -0300

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <div className="bg-[url('/image/temporario2.png')] bg-cover bg-center min-h-screen"></div>
+      <div className="bg-[url('/image/temporario2.png')] bg-cover bg-center min-h-screen" />
     </>
   );
 }
