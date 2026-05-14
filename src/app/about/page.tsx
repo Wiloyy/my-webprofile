117cf6bdbcc7ada1851423df97875a53eb3b5bc3
Wiloyy <mat.ams2006@gmail.com>
2026-05-14 20:32:47 -0300

    feat: add responsive about page with accessible layout

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,16 +7,16 @@ export default function About() {
     <>
       <div className="relative min-h-screen w-full overflow-hidden">
         <Background />
-        <div className="relative z-10 flex flex-col items-center w-full md:block">
+        <div className="relative z-10 flex flex-col md:flex-row items-center justify-center w-full gap-8 pt-20">
           <Image
             src={circuloBanco}
             width={300}
             height={300}
             alt="Círculo Branco"
-            className="w-[60%] top-25 md:w-[300px] h-auto relative mx-auto md:absolute md:left-[90px]"
+            className="w-[60%] md:w-[300px] h-auto"
           />
-          <div className="relative mx-auto md:mx-0 md:absolute top-20 right-4 md:right-[40px] lg:right-[80px] w-full max-w-[90%] md:w-[350px] lg:w-[450px] p-4">
-            <h1 className="text-4xl md:text-5xl font-bold text-white w-full text-center">
+          <div className="w-full max-w-[90%] md:w-[350px] lg:w-[450px] p-4">
+            <h1 className="text-4xl md:text-5xl font-bold text-white text-center">
               About me
             </h1>
             <p className="text-white text-base md:text-lg mt-5 text-justify">
