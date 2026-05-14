fec73038b3a6b837f960f2c64931cd05e0d9ef73
Wiloyy <mat.ams2006@gmail.com>
2026-05-14 20:25:05 -0300

    feat: add login responsivo com ajustes de acessibilidade e layout

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,48 @@
 import Background from "@/components/background";
+import Image from "next/image";
+import circuloBanco from "@/../public/image/circuloBranco.png";
 
 export default function Login() {
   return (
     <>
-      <div className="relative h-screen">
-        <Background />;
+      <div className="relative min-h-screen w-full overflow-hidden flex flex-col items-center justify-center">
+        <Background />
         <div className="relative z-10">
-          <h1>Voce esta no login</h1>
+          <Image
+            src={circuloBanco}
+            alt="Círculo Banco"
+            width={210}
+            height={210}
+            className=""
+          />
+          <div className="bg-transparent p-4">
+            <label htmlFor="email" className="sr-only">
+              Email
+            </label>
+            <input
+              id="email"
+              type="email"
+              placeholder="Email"
+              className="bg-transparent border-b border-white text-white placeholder:text-white focus:outline-none block mt-4 w-full"
+            />
+            <label htmlFor="password" className="sr-only">
+              Senha
+            </label>
+            <input
+              id="password"
+              type="password"
+              placeholder="Password"
+              className="bg-transparent border-b border-white text-white placeholder:text-white focus:outline-none block mt-4 w-full"
+            />
+            <div className="flex items-center flex-row gap-2">
+              <button className="bg-transparent border border-white text-white px-3 py-1 mt-2 text-sm hover:bg-white hover:text-black transition-colors duration-300">
+                Login
+              </button>
+              <button className="bg-transparent border border-white text-white px-3 py-1 mt-2 ml-auto text-sm hover:bg-white hover:text-black transition-colors duration-300">
+                Register
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <div className="bg-[url('/image/homeimagee.png')] bg-cover bg-center min-h-screen">
+      <div className="bg-[url('/image/temporario2.png')] bg-cover bg-center min-h-screen">
         <h1 className="text-4xl font-bold text-white text-center fixed top-170 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
           WEB DEVELOPER
         </h1>
